0492b940319828220528b60c678e9e8090c40ac3
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-02-01 16:56:21 -0300

    update index.html, create js
    add a js file with bank of questions
    add script to index. html

---
+++ index.js
@@ -0,0 +1,42 @@
+var questions = [
+    {
+        question : 'The 1998 world football championship was hosted in France, in which Brazil reached the final, winning 3-0' ,
+        answer : "false" ,
+        correct : 'The correct answer is: France won 3-0',
+    },
+     {
+        question : 'The Battle of Waterloo was a military confrontation that took place on 18 June 1815 near Waterloo, in present-day Belgium' ,
+        answer :  "true" ,
+        correct : 'The sentence is correct.',
+    },
+     {
+        question : 'Ayrton Senna, Brazilian Formula 1 driver, champion of the category in 1988, 1990 and 1991, died in a tragic accident on May 1, 1994 in Imola, Italy.' ,
+        answer : "true" ,
+        correct : 'The sentence is correct.',
+    },
+     {
+        question : 'U2 is an English rock band formed in 1976. The group is composed of Bono (vocals and guitar), The Edge (guitar, keyboards and backing vocals), Adam Clayton (bass) and Larry Mullen Jr. (drums and percussion )' ,
+        answer : "false" ,
+        correct : 'The correct answer is: U2 is an Irish rock band' ,
+    },
+     {
+        question : 'In June 1997, boxer Mike Tyson bit the ear of his opponent, Evander Holyfield. For his conduct, Tyson was fined, sentenced to community service and suspended from the sport for 1 year.' ,
+        answer : "true" ,
+        correct : 'The sentence is correct.',
+    },
+     {
+        question : 'The Chernobyl accident happened on April 26, 1986, when reactor 3 at the Chernobyl nuclear power plant exploded and released radioactive material into the atmosphere.' ,
+        answer :  "false" ,
+        correct : 'The correct answer is: The number 4 reactor exploded.',
+    },
+     {
+        question : 'Michael Jackson in 1986 released Thriller, the best-selling album in the history of music, considered a masterpiece. With the album, the future king of pop became the main black singer in the world and in the 1980s.' ,
+        answer : "false",
+        correct : 'The correct answer is: In 1982, Michael Jackson released Thriller',
+    },
+     {
+        question : 'The film "Avatar" is currently considered the highest-grossing film in the world.' ,
+        answer : "true" ,
+        correct : 'The sentence is correct.',
+    },
+];
